3efb9a0626b00416552dbd09a931a1c61bae66e3
zzuzzu0205 <zkakcnd@naver.com>
2023-05-20 16:19:28 +0900

    [Feat] update를 위한 함수 생성 후 버튼 연결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,44 @@
-export default function Home() {}
+import axios from "axios";
+
+export default function Home() {
+  const handleClick = async () => {
+    const request = new Request("username", "phone", "password");
+    const response: Response = await request.send();
+    console.log(response);
+  };
+
+  return (
+    <main>
+      <button style={{ width: "100px", height: "100px" }} onClick={handleClick}>
+        update
+      </button>
+    </main>
+  );
+}
+
+export class Request {
+  username?: string;
+  phone?: string;
+  password?: string;
+
+  constructor(username?: string, phone?: string, password?: string) {
+    this.username = username;
+    this.phone = phone;
+    this.password = password;
+  }
+  async send(): Promise<Response> {
+    return await axios.post("http://localhost:8080/user", this);
+  }
+}
+
+export class Response {
+  data: object;
+  StatusCode: number;
+  Message: string;
+
+  constructor(data: object, statusCode: number, message: string) {
+    this.data = data;
+    this.StatusCode = statusCode;
+    this.Message = message;
+  }
+}
