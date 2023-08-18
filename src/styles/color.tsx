cac1a86d52bfc793bf362c9492c351877964c5e3
JunYoung <wnsdud96330@gmail.com>
2023-08-18 13:20:32 +0900

    [Design] Font, Color 색상 emotion으로 정의

---
+++ src/styles/color.tsx
@@ -0,0 +1,51 @@
+export const black1ColorStyle = { 
+    backgroundColor: '#242533' 
+};
+export const black2ColorStyle = { 
+    backgroundColor: '#484964' 
+};
+export const black3ColorStyle = { 
+    backgroundColor: '#808192' 
+};
+export const gray1ColorStyle = { 
+    backgroundColor: '#ABACBE' 
+};
+export const gray2ColorStyle = { 
+    backgroundColor: '#BCBCC6' 
+};
+export const gray3ColorStyle = { 
+    backgroundColor: '#EEEEF2' 
+};
+export const purple2ColorStyle = { 
+    backgroundColor: '#644DED' 
+};
+export const purpl3ColorStyle = { 
+    backgroundColor: '#AD9FFF' 
+};
+export const purple4ColorStyle = { 
+    backgroundColor: '#644DED14' 
+};
+export const selagoColorStyle = { 
+    backgroundColor: '#E9E8FC' 
+};
+export const ivoryColorStyle = { 
+    backgroundColor: '#FAF3E1' 
+};
+export const blueColorStyle = { 
+    backgroundColor: '#60A9FF' 
+};
+export const lightgreenColorStyle = { 
+    backgroundColor: '#DBFF74' 
+};
+export const purple1ColorStyle = { 
+    backgroundColor: '#8960FF' 
+};
+export const white1ColorStyle = { 
+    backgroundColor: '#FFFFFF' 
+};
+export const white2ColorStyle = { 
+    backgroundColor: '#F3F3F8' 
+};
+export const white3ColorStyle = { 
+    backgroundColor: '#F9F9FC' 
+};
