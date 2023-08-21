977078d759332299787cbae2b85c172b41b4f68b
김진우 <jinwoo@gimjin-uui-MacBookPro.local>
2023-08-21 17:07:44 +0900

    [Feat] Kakao Login api 기능구현

---
+++ src/libs/kakao/kakaoLogin.ts
@@ -0,0 +1,62 @@
+import createRequest from "@/utils/instanceApi";
+import ResponseError, {isApiErrorResponse}  from "@/utils/ResponseError";
+import {KakaoAccessRequest, KakaoAccessResponse ,KakaoLoginRequest, KakaoLoginResponse} from "./kakao";
+import axios from "axios";
+
+
+const KAKAO_ACCESS_API_URL = 'https://kauth.kakao.com/oauth/authorize';
+const KAKAO_API_URL = 'https://kauth.kakao.com/oauth/token'
+
+
+export const accessLoginKakao = async ():Promise<KakaoAccessResponse> => {
+    try {
+        const client_id = 'REST_API_KEY'; 
+        const redirectUri = 'http://localhost:3000'; 
+
+        const params: KakaoAccessRequest = {
+            'client_id': client_id,
+            'redirect_uri': redirectUri,
+            'response_type': 'code',
+        };
+
+        const authorizeCode = await axios.get<KakaoAccessResponse>(KAKAO_ACCESS_API_URL, {
+            params,
+        });
+
+        return authorizeCode.data;
+    }
+    catch (e) {
+        if (isApiErrorResponse(e)) {
+          throw new ResponseError(e);
+        }
+        throw e;
+    }
+};
+
+export const loginWithKakao = async (authorizeCode:string):Promise<KakaoLoginResponse> => {
+    try{
+        const client_id = 'REST_API_KEY'; 
+        const redirectUri = 'http://localhost:3000'; 
+
+        const body: KakaoLoginRequest = {
+            'grant_type' : 'authorization_code',
+            'client_id': client_id,
+            'redirect_uri': redirectUri,
+            'code': authorizeCode,
+        };
+
+        const accessToken = await axios.post<KakaoLoginResponse>(KAKAO_API_URL, {
+            body,
+            headers : {
+                'Content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+            }
+        })
+
+        return accessToken.data
+    } catch (e) {
+        if (isApiErrorResponse(e)) {
+          throw new ResponseError(e);
+        }
+        throw e;
+    }
+}
